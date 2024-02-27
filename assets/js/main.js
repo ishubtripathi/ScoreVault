65c5d80ea462ef2fb2f2f7d240a9e287ef7dd82d
ishubtripathi <ishubtripathi@gmail.com>
2024-02-27 22:19:44 +0530

    Navbar Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,39 @@
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
+// /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+// const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+// function scrollActive(){
+//     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-            sectionTop = current.offsetTop - 50,
-            sectionId = current.getAttribute('id')
+//     sections.forEach(current =>{
+//         const sectionHeight = current.offsetHeight,
+//             sectionTop = current.offsetTop - 50,
+//             sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+//         }else{
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+//         }
+//     })
+// }
+// window.addEventListener('scroll', scrollActive)
+
+document.addEventListener("DOMContentLoaded", function() {
+    var url = window.location.href;
+    var activePage = url.substr(url.lastIndexOf('/') + 1); // Get the current page name
+
+    // Loop through each navigation item
+    var navItems = document.querySelectorAll('.nav__link');
+    navItems.forEach(function(item) {
+        var pageLink = item.getAttribute('href');
+        if (pageLink === activePage || pageLink === '/' + activePage) {
+            item.classList.add('active-link'); // Add 'active-link' class to the active page
         }
-    })
-}
-window.addEventListener('scroll', scrollActive)
+    });
+});
+
+
+
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
